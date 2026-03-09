70d1ddac6d5f04e9cba52686e8f29b9ec39dfc08
WindBreeze1 <110368127+WindBreeze1@users.noreply.github.com>
2026-03-09 15:49:09 -0700

    updated to add base in .mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,9 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
-    site: 'https://windbreeze1.github.io/allendb.github.io',
+  site: 'https://windbreeze1.github.io',
+  base: '/', // important for user site
   vite: {
     plugins: [tailwindcss()],
   },
