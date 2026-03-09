fdb44997b53fb3395f33374a14273618ad6d9ff1
WindBreeze1 <110368127+WindBreeze1@users.noreply.github.com>
2026-03-09 14:55:48 -0700

    Fix GitHub Deploy, added yml file

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://windbreeze1.github.io/allendb.github.io/',
+    base: '/allendb.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
