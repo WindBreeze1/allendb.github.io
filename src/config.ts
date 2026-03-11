d37019be04b654893ef04d42f5c8888c709ad393
WindBreeze1 <110368127+WindBreeze1@users.noreply.github.com>
2026-03-10 20:15:45 -0700

    update config.ts
    link for TA evals

--- src/config.ts
+++ src/config.ts
@@ -47,7 +47,7 @@ skills: ["SAP2000", "MATLAB", "SolidWorks", "Python", "CAD"],
   {
     name: "Teaching Assistant Evaluations",
     description: "Evaluations from students and the professor when I was a teaching assistant at UC San Diego for SE 151A.",
-    link: "/public/Projects/Student_Evaluations_Reviews.pdf",
+    link: "/Projects/Student_Evaluations_Reviews.pdf",
   }
 ],
   experience: [
